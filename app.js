bad446f64effda9b7e8c36e977facc37668483fa
kuldmitri <KulDmitriBy@gmail.com>
2017-10-15 21:03:05 +0300

    code formatting

--- app.js
+++ app.js
@@ -7,50 +7,52 @@ var jsonParser = bodyParser.json();
 
 app.use(express.static(__dirname + "/public"));
 // получение списка данных 1111111111
-app.get("/api/users", function(req, res){
-	
+app.get("/api/users", function (req, res) {
+
     var content = fs.readFileSync("users.json", "utf8");
     var users = JSON.parse(content);
     res.send(users);
 });
 // получение одного пользователя по id
-app.get("/api/users/:id", function(req, res){
-	
+app.get("/api/users/:id", function (req, res) {
+
     var id = req.params.id; // получаем id
     var content = fs.readFileSync("users.json", "utf8");
     var users = JSON.parse(content);
     var user = null;
     // находим в массиве пользователя по id
-    for(var i=0; i<users.length; i++){
-		if(users[i].id==id){
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].id == id) {
             user = users[i];
             break;
-		}
-	}
+        }
+    }
     // отправляем пользователя
-    if(user){
+    if (user) {
         res.send(user);
-	}
-    else{
+    }
+    else {
         res.status(404).send();
-	}
+    }
 });
 // получение отправленных данных
 app.post("/api/users", jsonParser, function (req, res) {
-	
-    if(!req.body) return res.sendStatus(400);
-	
+
+    if (!req.body) return res.sendStatus(400);
+
     var userName = req.body.name;
     var userAge = req.body.age;
     var user = {name: userName, age: userAge};
-	
+
     var data = fs.readFileSync("users.json", "utf8");
     var users = JSON.parse(data);
-	
+
     // находим максимальный id
-    var id = Math.max.apply(Math,users.map(function(o){return o.id;}))
+    var id = Math.max.apply(Math, users.map(function (o) {
+        return o.id;
+    }))
     // увеличиваем его на единицу
-    user.id = id+1;
+    user.id = id + 1;
     // добавляем пользователя в массив
     users.push(user);
     var data = JSON.stringify(users);
@@ -59,62 +61,62 @@ app.post("/api/users", jsonParser, function (req, res) {
     res.send(user);
 });
 // удаление пользователя по id
-app.delete("/api/users/:id", function(req, res){
-	
+app.delete("/api/users/:id", function (req, res) {
+
     var id = req.params.id;
     var data = fs.readFileSync("users.json", "utf8");
     var users = JSON.parse(data);
     var index = -1;
     // находим индекс пользователя в массиве
-    for(var i=0; i<users.length; i++){
-        if(users[i].id==id){
-            index=i;
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].id == id) {
+            index = i;
             break;
-		}
-	}
-    if(index > -1){
+        }
+    }
+    if (index > -1) {
         // удаляем пользователя из массива по индексу
         var user = users.splice(index, 1)[0];
         var data = JSON.stringify(users);
         fs.writeFileSync("users.json", data);
         // отправляем удаленного пользователя
         res.send(user);
-	}
-    else{
+    }
+    else {
         res.status(404).send();
-	}
+    }
 });
 // изменение пользователя
-app.put("/api/users", jsonParser, function(req, res){
-	
-    if(!req.body) return res.sendStatus(400);
-	
+app.put("/api/users", jsonParser, function (req, res) {
+
+    if (!req.body) return res.sendStatus(400);
+
     var userId = req.body.id;
     var userName = req.body.name;
     var userAge = req.body.age;
-	
+
     var data = fs.readFileSync("users.json", "utf8");
     var users = JSON.parse(data);
     var user;
-    for(var i=0; i<users.length; i++){
-        if(users[i].id==userId){
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].id == userId) {
             user = users[i];
             break;
-		}
-	}
+        }
+    }
     // изменяем данные у пользователя
-    if(user){
+    if (user) {
         user.age = userAge;
         user.name = userName;
         var data = JSON.stringify(users);
         fs.writeFileSync("users.json", data);
         res.send(user);
-	}
-    else{
+    }
+    else {
         res.status(404).send(user);
-	}
+    }
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Сервер ожидает подключения...");
 });
